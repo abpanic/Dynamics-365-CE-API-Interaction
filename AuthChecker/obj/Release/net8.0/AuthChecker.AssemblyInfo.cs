ef22b762b38e3f3a273411aa788616bb0a06b6dc
Abhilash <abhilash.panicker@outlook.com>
2025-11-14 01:27:36 +0530

    update to higher version

--- AuthChecker/obj/Release/net8.0/AuthChecker.AssemblyInfo.cs
+++ AuthChecker/obj/Release/net8.0/AuthChecker.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Abhilash Panicker")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Debug OAuth related access issues")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15b87ae0a6045ac9f20edd9d45467be0dae4444a")]
